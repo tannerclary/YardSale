56d933ff9ebe6f40ef697a7cac77ba0769c00138
Tanner Clary <tanman@live.unc.edu>
2022-05-06 18:26:46 -0400

    garbage gonna do what garbage does

--- yardsale/src/App.js
+++ yardsale/src/App.js
@@ -1,18 +1,28 @@
 import logo from './logo.svg';
+import './descriptions.json';
 
 import './App.css';
 
 function App() {
-  let num = 1;
-  return (
-    <div className="App">
-      <header className="App-header">
-      <img src={require('./assets/testPic' + num + '.png')}/>
-        <p>1</p>
-        
-      </header>
-    </div>
-  );
+  const descriptions = require('./descriptions.json');
+
+  function getImage(id) {
+    
+    return (
+      <div class="container">
+        <img src={require('./assets/realPic' + id + '.JPG')}/>
+        <p className="itemID">{id}</p>
+        <p class="itemID">{descriptions.Descriptions[id - 1].description}</p>
+      </div>);
+  }
+
+  var myData = [1,2];
+
+  const children = myData.map((val) => (
+    getImage(val)
+  ));
+
+  return (<div class="wrapper">{children}</div>)
 }
 
 export default App;
